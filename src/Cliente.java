6132e3fece90d67a575815e97218282e95f8f900
felipecesar12 <62731849+felipecesar12@users.noreply.github.com>
2020-03-27 19:27:02 -0300

    Add files via upload

---
+++ src/Cliente.java
@@ -0,0 +1,33 @@
+public class Cliente {
+
+
+    String nome;
+    String sobrenome;
+    int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) { this.idade = idade; }
+
+}
+
+
+
---
+++ src/DadosConta.java
@@ -0,0 +1,18 @@
+public class DadosConta {
+
+    String numeroAgencia;
+    String numeroConta;
+    int codigoBanco;
+
+    public String getNumeroAgencia() {return numeroAgencia;}
+
+    public void setNumeroAgencia(String numeroAgencia) {this.numeroAgencia = numeroAgencia;}
+
+    public String getNumeroConta() {return numeroConta;}
+
+    public void setNumeroConta(String numeroConta) {this.numeroConta = numeroConta;}
+
+    public int getCodigoBanco() {return codigoBanco;}
+
+    public void setCodigoBanco(int codigoBanco) {this.codigoBanco = codigoBanco;}
+}
