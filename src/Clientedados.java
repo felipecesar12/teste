a7d76945e355665b4735ac5d7c128de335ab6858
felipecesar12 <62731849+felipecesar12@users.noreply.github.com>
2020-03-27 19:55:00 -0300

    Add files via upload

---
+++ src/Clientedados.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Clientedados {
+
+    public static void main(String[] args) {
+
+        Cliente negoVeio1 = criaCliente();
+        DadosConta DADOSnegoVeio1 = criaDados();
+
+
+        System.out.println("O "+negoVeio1.getNome()+ " cujo sobrenome é "+negoVeio1.getSobrenome()+
+                " e sua idade é "+negoVeio1.getIdade()+ " tem os seguintes dados bancários: "+
+                "\n"+"Agencia:"+DADOSnegoVeio1.getNumeroAgencia()+
+                "\n"+"Conta: "+DADOSnegoVeio1.getNumeroConta()+
+                "\n"+"Codigo Banco: "+DADOSnegoVeio1.getCodigoBanco());
+
+    }
+
+    public static Cliente criaCliente() {
+        Cliente negoVeio1 = new Cliente();
+        negoVeio1.setNome(digita("Nome: "));
+        negoVeio1.setSobrenome(digita("Sobrenome: "));
+        negoVeio1.setIdade(Integer.parseInt(digita("Idade: ")));
+        return negoVeio1;
+    }
+        public static  DadosConta criaDados(){
+        DadosConta DADOSnegoVeio1 = new DadosConta();
+            DADOSnegoVeio1.setNumeroAgencia(digita("Agência: "));
+            DADOSnegoVeio1.setNumeroConta(digita("Conta: "));
+            DADOSnegoVeio1.setCodigoBanco(Integer.parseInt(digita("Codigo Banco: ")));
+        return DADOSnegoVeio1;
+    }
+
+    public static String digita(String mens) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println(mens);
+        return teclado.next();
+
+    }
+
+
+
+}// FINAL
